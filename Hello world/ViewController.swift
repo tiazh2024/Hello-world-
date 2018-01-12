c8c02aef22f89d304857ae3e809065f9dbad542e
tianchen zhang <tiazh2024@andys-macbook-pro.local>
2018-01-12 13:54:42 +0800

    Initial Commit

---
+++ Hello world/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello world
+//
+//  Created by tianchen zhang on 12/1/18.
+//  Copyright © 2018 tianchen zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
